0345024cfcf84770d9c291c8a05dea48831b7025
Mustafanoorahmadi <noorahmadimustafa204@gmail.com>
2024-06-30 20:41:02 +0430

    step 1

---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,20 @@
+import SendOtpForm from "../components/templates/SendOtpForm";
+import CheckOtpForm from "../components/templates/CheckOtpForm";
+import { useState } from "react";
+
+function AuthPage() {
+  const [step, setStep] = useState(1);
+  const [mobile, setMobile] = useState("");
+  const [code, setCode] = useState("");
+
+  return (
+    <div>
+      {step === 1 && (
+        <SendOtpForm setStep={setStep} mobile={mobile} setMobile={setMobile} />
+      )}
+      {step === 2 && <CheckOtpForm />}
+    </div>
+  );
+}
+
+export default AuthPage;
